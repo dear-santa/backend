e15f47c9ef3dbcfec5f638f34bd7b4f279683342
sooyoungh <sooyoung.h8@gmail.com>
2023-12-08 01:04:12 +0900

    feat: 게시물 전체 조회 기능 구현
    - 대/소분류
    - 페이징
    - 정렬순

--- src/main/java/com/dearsanta/app/mapper/BoardMapper.java
+++ src/main/java/com/dearsanta/app/mapper/BoardMapper.java
@@ -1,7 +1,13 @@
 package com.dearsanta.app.mapper;
 
 import com.dearsanta.app.dto.BoardDto;
+import com.dearsanta.app.dto.Criteria;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface BoardMapper {
     BoardDto getBoard(String boardId);
+
+    List<BoardDto> getBoardListWithPaging(@Param("criteria")Criteria criteria);
 }
--- src/main/java/com/dearsanta/app/service/BoardServiceImpl.java
+++ src/main/java/com/dearsanta/app/service/BoardServiceImpl.java
@@ -1,15 +1,47 @@
 package com.dearsanta.app.service;
 
+import com.dearsanta.app.domain.enumtype.Sorted;
 import com.dearsanta.app.dto.BoardDto;
+import com.dearsanta.app.dto.BoardListDto;
+import com.dearsanta.app.dto.Criteria;
+import com.dearsanta.app.mapper.BoardCategoryMapper;
 import com.dearsanta.app.mapper.BoardMapper;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Log4j
 @Service
 public class BoardServiceImpl implements BoardService{
 
     @Autowired
-    BoardMapper boardMapper;
+    private BoardMapper boardMapper;
+    @Autowired
+    private BoardCategoryMapper boardCategoryMapper;
+
+    @Override
+    public BoardListDto getBoardListWithPaging(
+            String mainCategory, String subCategory, int pageNum, int pageSize, Sorted sorted
+    ) {
+        String categoryId = findBoardCategoryId(mainCategory, subCategory);
+        log.info("pageNum:" + pageNum + " pageSize: " + pageSize + " sort: " + sorted.getIndexColumn());
+        Criteria criteria = new Criteria(categoryId, pageNum, pageSize, sorted.getIndexColumn());
+        List<BoardDto> boardDtos = boardMapper.getBoardListWithPaging(criteria);
+        return new BoardListDto(boardDtos);
+    }
+
+    private String findBoardCategoryId(String mainCategory, String subCategory) {
+        String categoryId = boardCategoryMapper.getBoardCategoryId(mainCategory, subCategory);
+        log.info("{mainCategory: " + mainCategory + ", subCategory: " + subCategory + "} => categoryId=" + categoryId);
+
+        if (categoryId == null) {
+            throw new NoSuchElementException("해당하는 게시판이 존재하지 않습니다.");
+        }
+        return categoryId;
+    }
 
     @Override
     public BoardDto getBoard(String boardId) {
